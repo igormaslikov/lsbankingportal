a6807e3b091a1c6dfe5ebc406ac4a7cd86366068
Igor <maslikov.igor@gmail.com>
2021-06-28 21:53:18 +0300

    change http to https in link and src hrefs

--- signature_commercial_loan/completed/index.php
+++ signature_commercial_loan/completed/index.php
@@ -1,8 +1,8 @@
 <?php
-$url_logo="https://lsbankingportal.com/signature_commercial_loan/completed"; 
+$url_logo = "https://lsbankingportal.com/signature_commercial_loan/completed";
 include 'dbconnect.php';
 include 'dbconfig.php';
-$iddd=$_GET['id'];
+$iddd = $_GET['id'];
 // echo "idddd". $iddd;
 
 //echo "key is".$mail_key;
@@ -12,207 +12,201 @@ $iddd=$_GET['id'];
 
 
 
-$sql1=mysqli_query($con, "select * from commercial_loan_initial_banking where email_key='$iddd' "); 
+$sql1 = mysqli_query($con, "select * from commercial_loan_initial_banking where email_key='$iddd' ");
 
-while($row1 = mysqli_fetch_array($sql1)) {
+while ($row1 = mysqli_fetch_array($sql1)) {
 
-$mail_key=$row1['email_key'];
-$signed_status=$row1['sign_status'];
-$creation_date=$row1['creation_date'];
-$fnd_id=$row1['user_fnd_id'];
-$loan_id_bor=$row1['loan_id'];
-$type_of_card=$row1['type_of_card'];
-$card_number=$row1['card_number'];
-$card_exp_date=$row1['card_exp_date'];
+	$mail_key = $row1['email_key'];
+	$signed_status = $row1['sign_status'];
+	$creation_date = $row1['creation_date'];
+	$fnd_id = $row1['user_fnd_id'];
+	$loan_id_bor = $row1['loan_id'];
+	$type_of_card = $row1['type_of_card'];
+	$card_number = $row1['card_number'];
+	$card_exp_date = $row1['card_exp_date'];
 
-$bank_name=$row1['bank_name'];
-$routing_number=$row1['routing_number'];
-$account_number=$row1['account_number'];
+	$bank_name = $row1['bank_name'];
+	$routing_number = $row1['routing_number'];
+	$account_number = $row1['account_number'];
 
-$cvv_number=$row1['cvv_number'];
+	$cvv_number = $row1['cvv_number'];
 
-$img_signed = $row1['signed_pic'];
+	$img_signed = $row1['signed_pic'];
 
-$result_sig = $url_logo .'/doc_signs/'. $img_signed;
+	$result_sig = $url_logo . '/doc_signs/' . $img_signed;
 }
 
-if($signed_status>0){
-    echo "Contract Already Signed";
-}
-else {
-//echo "ID is".$loan_id;
+if ($signed_status > 0) {
+	echo "Contract Already Signed";
+} else {
+	//echo "ID is".$loan_id;
 
 
 
-$sql_loan=mysqli_query($con, "select * from tbl_commercial_loan where loan_id= '$loan_id_bor' "); 
+	$sql_loan = mysqli_query($con, "select * from tbl_commercial_loan where loan_id= '$loan_id_bor' ");
 
-while($row_loan = mysqli_fetch_array($sql_loan)) {
-    
-    
-    $amount_of_loan=$row_loan['amount_of_loan'];
-    $payment_date=$row_loan['payment_date'];
-    // echo "LOAN Amount".$amount_of_loan;
-    
-   
-    
-     
-    $payoff=$row_loan['loan_total_payable'];
-    
-   
-        
-        
-        
-        
-}
+	while ($row_loan = mysqli_fetch_array($sql_loan)) {
+
+
+		$amount_of_loan = $row_loan['amount_of_loan'];
+		$payment_date = $row_loan['payment_date'];
+		// echo "LOAN Amount".$amount_of_loan;
 
 
 
 
-$sql2=mysqli_query($con, "select * from fnd_user_profile where user_fnd_id='$fnd_id' "); 
-
-while($row2 = mysqli_fetch_array($sql2)) {
-
-$f_name=$row2['first_name'];
-$address=$row2['address'];
-$city=$row2['city'];
-$state=$row2['state'];
-$zip=$row2['zip_code'];
-$mobile_number=$row2['mobile_number'];
-$address=$row2['address'];
-
-
-
-
-}
-
-    $search_dir = "doc_signs/$result";
-   $images = glob("$search_dir/*.png");
-   sort($images);
-
-   // Image selection and display:
-
-   //display first image
-   if (count($images) > 0) { // make sure at least one image exists
-       $img = $images[0]; // first image
-      // echo "<img src='$img' height='150' width='150' /> ";
-   } else {
-       // possibly display a placeholder image?
-   }
-?>
-
-
-
-
-
-
-<?php
-	$date1="$creation_date";
-	$date2="$payment_date";
-	function dateDiff($date1, $date2) 
-	{
-	  $date1_ts = strtotime($date1);
-	  $date2_ts = strtotime($date2);
-	  $diff = $date2_ts - $date1_ts;
-	  return round($diff / 86400);
+		$payoff = $row_loan['loan_total_payable'];
 	}
-	$dateDiff= dateDiff($date1, $date2);
-// echo "Days".$dateDiff."<br>";
 
 
-$payoff=str_replace('$', '', $payoff);
-$amount_of_loan=str_replace('$', '', $amount_of_loan);
-$apr=$payoff/$amount_of_loan;
-$apr_total=$apr*365;
-$anual_pr=($apr_total/$dateDiff)*100;
-	//echo $anual_pr;
+
+
+	$sql2 = mysqli_query($con, "select * from fnd_user_profile where user_fnd_id='$fnd_id' ");
+
+	while ($row2 = mysqli_fetch_array($sql2)) {
+
+		$f_name = $row2['first_name'];
+		$address = $row2['address'];
+		$city = $row2['city'];
+		$state = $row2['state'];
+		$zip = $row2['zip_code'];
+		$mobile_number = $row2['mobile_number'];
+		$address = $row2['address'];
+	}
+
+	$search_dir = "doc_signs/$result";
+	$images = glob("$search_dir/*.png");
+	sort($images);
+
+	// Image selection and display:
+
+	//display first image
+	if (count($images) > 0) { // make sure at least one image exists
+		$img = $images[0]; // first image
+		// echo "<img src='$img' height='150' width='150' /> ";
+	} else {
+		// possibly display a placeholder image?
+	}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contract Signature</title>
-    
-    <!-- Bootstrap Core Css -->
-    <link href="css/bootstrap.css" rel="stylesheet" />
 
-    <!-- Font Awesome Css -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
 
-	<!-- Bootstrap Select Css -->
-    <link href="css/bootstrap-select.css" rel="stylesheet" />
 
-    <!-- Custom Css -->
-    <link href="css/app_style.css" rel="stylesheet" />
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<style>
-		
-		#btnSaveSign {
-			color: #fff;
-			background: #f99a0b;
-			padding: 5px;
-			border: none;
-			border-radius: 5px;
-			font-size: 20px;
-			margin-top: 10px;
-		}
-		
-		
-		#btnupload {
-		    color: #fff;
-			background: #f99a0b;
-			padding: 5px;
-			border: none;
-			border-radius: 5px;
-			font-size: 20px;
-			margin-top: 10px;
-		}
-		
-		
-		#signArea{
-			width:304px;
-			margin: 15px auto;
-		}
-		.sign-container {
-			width: 90%;
-			margin: auto;
-		}
-		.sign-preview {
-			width: 150px;
-			height: 50px;
-			border: solid 1px #CFCFCF;
-			margin: 10px 5px;
-		}
-		.tag-ingo {
-			font-family: cursive;
-			font-size: 12px;
-			text-align: left;
-			font-style: oblique;
-		}
-		.center-text {
-			text-align: center;
-		}
-	</style>
-	
-	
-	
-	
-	
-	
-</head>
-<body>
-    <div class="all-content-wrapper">
-		<!-- Top Bar -->
-		<?php require_once('./include/header.php'); ?>
-		<!-- #END# Top Bar -->
-	
-		<section class="container">
-		
-				
+
+
+	<?php
+	$date1 = "$creation_date";
+	$date2 = "$payment_date";
+	function dateDiff($date1, $date2)
+	{
+		$date1_ts = strtotime($date1);
+		$date2_ts = strtotime($date2);
+		$diff = $date2_ts - $date1_ts;
+		return round($diff / 86400);
+	}
+	$dateDiff = dateDiff($date1, $date2);
+	// echo "Days".$dateDiff."<br>";
+
+
+	$payoff = str_replace('$', '', $payoff);
+	$amount_of_loan = str_replace('$', '', $amount_of_loan);
+	$apr = $payoff / $amount_of_loan;
+	$apr_total = $apr * 365;
+	$anual_pr = ($apr_total / $dateDiff) * 100;
+	//echo $anual_pr;
+	?>
+
+	<!DOCTYPE html>
+	<html lang="en">
+
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Contract Signature</title>
+
+		<!-- Bootstrap Core Css -->
+		<link href="css/bootstrap.css" rel="stylesheet" />
+
+		<!-- Font Awesome Css -->
+		<link href="css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Bootstrap Select Css -->
+		<link href="css/bootstrap-select.css" rel="stylesheet" />
+
+		<!-- Custom Css -->
+		<link href="css/app_style.css" rel="stylesheet" />
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<style>
+			#btnSaveSign {
+				color: #fff;
+				background: #f99a0b;
+				padding: 5px;
+				border: none;
+				border-radius: 5px;
+				font-size: 20px;
+				margin-top: 10px;
+			}
+
+
+			#btnupload {
+				color: #fff;
+				background: #f99a0b;
+				padding: 5px;
+				border: none;
+				border-radius: 5px;
+				font-size: 20px;
+				margin-top: 10px;
+			}
+
+
+			#signArea {
+				width: 304px;
+				margin: 15px auto;
+			}
+
+			.sign-container {
+				width: 90%;
+				margin: auto;
+			}
+
+			.sign-preview {
+				width: 150px;
+				height: 50px;
+				border: solid 1px #CFCFCF;
+				margin: 10px 5px;
+			}
+
+			.tag-ingo {
+				font-family: cursive;
+				font-size: 12px;
+				text-align: left;
+				font-style: oblique;
+			}
+
+			.center-text {
+				text-align: center;
+			}
+		</style>
+
+
+
+
+
+
+	</head>
+
+	<body>
+		<div class="all-content-wrapper">
+			<!-- Top Bar -->
+			<?php require_once('./include/header.php'); ?>
+			<!-- #END# Top Bar -->
+
+			<section class="container">
+
+
 				<div class="page-body clearfix">
 					<div class="row">
 						<div class="col-md-offset-1 col-md-10">
@@ -220,83 +214,79 @@ $anual_pr=($apr_total/$dateDiff)*100;
 								<div class="panel-heading">Digital Signature:</div>
 								<div class="panel-body center-text">
 
-								<div id="signArea" >
-									<h2 class="tag-ingo">Put signature below,</h2>
-									<div class="sig sigWrapper" style="height:auto;">
-										<div class="typed"></div>
-										<canvas class="sign-pad" id="sign-pad" width="300" height="100"></canvas>
+									<div id="signArea">
+										<h2 class="tag-ingo">Put signature below,</h2>
+										<div class="sig sigWrapper" style="height:auto;">
+											<div class="typed"></div>
+											<canvas class="sign-pad" id="sign-pad" width="300" height="100"></canvas>
+										</div>
 									</div>
-								</div>
-								
-								
-						<button id="btnSaveSign">Save Signature</button> <br>
-								<b>OR</b> 
-								<br>
-								
-<?php
 
-include 'dbconnect.php';
-include 'dbconfig.php';
 
-if(isset($_POST['btnupload'])) 
-{
-        $imgFile = $_FILES['imagee']['name'];
-        $tmp_dir = $_FILES['imagee']['tmp_name'];
-        $imgSize = $_FILES['imagee']['size'];
-        
-            $upload_dir = 'doc_signs/'; // upload directory
-    
-            $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension
-        
-            // valid image extensions
-            $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
-        
-            // rename uploading image
-            $userpic = rand(1000,1000000).".".$imgExt;
-                
-            // allow valid image file formats
-            if(in_array($imgExt, $valid_extensions)){            
-                // Check file size '5MB'
-                if($imgSize < 5000000)                {
-                    move_uploaded_file($tmp_dir,$upload_dir.$userpic);
-                }
-                else{
-                    $errMSG = "Sorry, your file is too large.";
-                }
-            }
-            else{
-                $errMSG = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";        
-            }
+									<button id="btnSaveSign">Save Signature</button> <br>
+									<b>OR</b>
+									<br>
 
- $query_sign  = "UPDATE commercial_loan_initial_banking SET `sign_status`='1',`signed_pic`='$userpic' WHERE `email_key` = '$iddd' ";
-        $result_sign = mysqli_query($con, $query_sign);
-        if ($result_sign) {
-            //echo "<div class='form'><h3> successfully added in tbl_shipments.</h3><br/></div>";
-        } else {
-        echo "<h3> Error Inserting Data </h3>";
-        }
+									<?php
 
-}
-?>
-								
-		<form action ="" method="POST" enctype="multipart/form-data">						
-       <label for="usr">Upload Your Signature</label>
-       <input type="file" name="imagee"  class="form-control" accept="image/*" style="width:35%;margin-left:33%"><br>
-       <button id="btnupload" type="submit" name="btnupload"> Upload Signature</button>
-		</form>
-		
-								<div class="sign-container" style="display:none;">
-								<?php
-								$image_list = glob("./doc_signs/*.png");
-								foreach($image_list as $image){
-									//echo $image;
-								?>
-								<img src="<?php echo $image; ?>" class="sign-preview" />
-								<?php
-								}
-								?>
-								</div>
-									
+									include 'dbconnect.php';
+									include 'dbconfig.php';
+
+									if (isset($_POST['btnupload'])) {
+										$imgFile = $_FILES['imagee']['name'];
+										$tmp_dir = $_FILES['imagee']['tmp_name'];
+										$imgSize = $_FILES['imagee']['size'];
+
+										$upload_dir = 'doc_signs/'; // upload directory
+
+										$imgExt = strtolower(pathinfo($imgFile, PATHINFO_EXTENSION)); // get image extension
+
+										// valid image extensions
+										$valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+
+										// rename uploading image
+										$userpic = rand(1000, 1000000) . "." . $imgExt;
+
+										// allow valid image file formats
+										if (in_array($imgExt, $valid_extensions)) {
+											// Check file size '5MB'
+											if ($imgSize < 5000000) {
+												move_uploaded_file($tmp_dir, $upload_dir . $userpic);
+											} else {
+												$errMSG = "Sorry, your file is too large.";
+											}
+										} else {
+											$errMSG = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+										}
+
+										$query_sign  = "UPDATE commercial_loan_initial_banking SET `sign_status`='1',`signed_pic`='$userpic' WHERE `email_key` = '$iddd' ";
+										$result_sign = mysqli_query($con, $query_sign);
+										if ($result_sign) {
+											//echo "<div class='form'><h3> successfully added in tbl_shipments.</h3><br/></div>";
+										} else {
+											echo "<h3> Error Inserting Data </h3>";
+										}
+									}
+									?>
+
+									<form action="" method="POST" enctype="multipart/form-data">
+										<label for="usr">Upload Your Signature</label>
+										<input type="file" name="imagee" class="form-control" accept="image/*" style="width:35%;margin-left:33%"><br>
+										<button id="btnupload" type="submit" name="btnupload"> Upload Signature</button>
+									</form>
+
+									<div class="sign-container" style="display:none;">
+										<?php
+										$image_list = glob("./doc_signs/*.png");
+										foreach ($image_list as $image) {
+											//echo $image;
+										?>
+											<img src="<?php echo $image; ?>" class="sign-preview" />
+										<?php
+										}
+										?>
+									</div>
+
 
 								</div>
 							</div>
@@ -305,91 +295,101 @@ if(isset($_POST['btnupload']))
 
 					</div>
 				</div>
-			
-				
-			</div>
+
+
+		</div>
 		</section>
-    </div>
-	
-	<!-- Jquery Core Js -->
-    <script src="js/jquery.min.js"></script>
+		</div>
 
-    <!-- Bootstrap Core Js -->
-    <script src="js/bootstrap.min.js"></script>
+		<!-- Jquery Core Js -->
+		<script src="js/jquery.min.js"></script>
 
-    <!-- Bootstrap Select Js -->
-    <script src="js/bootstrap-select.js"></script>
+		<!-- Bootstrap Core Js -->
+		<script src="js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-	
-	<link href="./css/jquery.signaturepad.css" rel="stylesheet">
-	<script src="./js/numeric-1.2.6.min.js"></script> 
-	<script src="./js/bezier.js"></script>
-	<script src="./js/jquery.signaturepad.js"></script> 
-	
-	<script type='text/javascript' src="https://github.com/niklasvh/html2canvas/releases/download/0.4.1/html2canvas.js"></script>
-	<script src="./js/json2.min.js"></script>
-	
-	<script>
+		<!-- Bootstrap Select Js -->
+		<script src="js/bootstrap-select.js"></script>
 
-	$(document).ready(function(e){
+		<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
-		$(document).ready(function() {
-			$('#signArea').signaturePad({drawOnly:true, drawBezierCurves:true, lineTop:90});
-		});
-		
-		$("#btnSaveSign").click(function(e){
-			html2canvas([document.getElementById('sign-pad')], {
-				onrendered: function (canvas) {
-					var canvas_img_data = canvas.toDataURL('image/png');
-					var img_data = canvas_img_data.replace(/^data:image\/(png|jpg);base64,/, "");
-					var key = '<?php echo $iddd;?>';
-					//ajax call to save image inside folder
-					$.ajax({
-						url: 'save_sign.php',
-						data: { img_data:img_data, key:key },
-						type: 'post',
-						dataType: 'json',
-						success: function (response) {
-						   window.location.href="../files/sign_contract.php?id=<?php echo $iddd; ?>";
+		<link href="./css/jquery.signaturepad.css" rel="stylesheet">
+		<script src="./js/numeric-1.2.6.min.js"></script>
+		<script src="./js/bezier.js"></script>
+		<script src="./js/jquery.signaturepad.js"></script>
+
+		<script type='text/javascript' src="https://github.com/niklasvh/html2canvas/releases/download/0.4.1/html2canvas.js"></script>
+		<script src="./js/json2.min.js"></script>
+
+		<script>
+			$(document).ready(function(e) {
+
+				$(document).ready(function() {
+					$('#signArea').signaturePad({
+						drawOnly: true,
+						drawBezierCurves: true,
+						lineTop: 90
+					});
+				});
+
+				$("#btnSaveSign").click(function(e) {
+					html2canvas([document.getElementById('sign-pad')], {
+						onrendered: function(canvas) {
+							var canvas_img_data = canvas.toDataURL('image/png');
+							var img_data = canvas_img_data.replace(/^data:image\/(png|jpg);base64,/, "");
+							var key = '<?php echo $iddd; ?>';
+							//ajax call to save image inside folder
+							$.ajax({
+								url: 'save_sign.php',
+								data: {
+									img_data: img_data,
+									key: key
+								},
+								type: 'post',
+								dataType: 'json',
+								success: function(response) {
+									window.location.href = "../files/sign_contract.php?id=<?php echo $iddd; ?>";
+								},
+								error: function(res){
+									console.log(res);
+								}
+							});
 						}
 					});
-				}
+				});
+
 			});
-		});
-
-	});
-	</script>
-	
-	
-	
-	
-	
-	
-	
-	
-</body>
-</html>
-
-<?php
- 
-include 'dbconnect.php';
-include 'dbconfig.php';
-$iddd=$_GET['id'];
-$sql_link=mysqli_query($con, "select * from commercial_loan_initial_banking where email_key='$iddd' "); 
-
-while($row_link = mysqli_fetch_array($sql_link)) {
-    
-    $mail_key_link=$row_link['email_key'];
-    //echo $mail_key_link;
-}
-
-$url_link="https://lsbankingportal.com/signature_commercial_loan/files/index.php?id=$mail_key_link";
-
-?>
+		</script>
 
 
 
-<iframe src="<?php echo $url_link; ?>" width="100%" height="900"></iframe>
+
+
+
+
+
+	</body>
+
+	</html>
+
+	<?php
+
+	include 'dbconnect.php';
+	include 'dbconfig.php';
+	$iddd = $_GET['id'];
+	$sql_link = mysqli_query($con, "select * from commercial_loan_initial_banking where email_key='$iddd' ");
+
+	while ($row_link = mysqli_fetch_array($sql_link)) {
+
+		$mail_key_link = $row_link['email_key'];
+		//echo $mail_key_link;
+	}
+
+	$url_link = "https://lsbankingportal.com/signature_commercial_loan/files/index.php?id=$mail_key_link";
+
+	?>
+
+
+
+	<iframe src="<?php echo $url_link; ?>" width="100%" height="900"></iframe>
 <?php } ?>
