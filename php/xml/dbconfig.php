45a19e028284dc460757f9283d190d2984672eab
Igor <maslikov.igor@gmail.com>
2021-07-21 08:35:06 +0300

    fix connection to dbs64065

--- php/xml/dbconfig.php
+++ php/xml/dbconfig.php
@@ -1,11 +1,11 @@
 <?php
-$host = "mymoneyline.com/lsbankingportal/"; 
-$user = "db2lsuser2021"; 
-$password = "^%D24L*!Ti5%"; 
-$database = "dbs64065"; 
+$db_host = "50.62.151.36"; 
+$db_user = "db2lsuser2021"; 
+$db_pass = "^%D24L*!Ti5%"; 
+$db_name = "dbs64065"; 
 
 // Connect to MySQL Database
-include_once $_SERVER['DOCUMENT_ROOT'].'/dbconnection.php';
+//include_once $_SERVER['DOCUMENT_ROOT'].'/dbconnection.php';
 $con = new mysqli($db_host,$db_user,$db_pass,$db_name);
 
 // Check connection
