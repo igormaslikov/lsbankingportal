e5c4f6ffb4666e9cd76d496455000cfbbe3a7daa
Igor <maslikov.igor@gmail.com>
2021-08-10 01:50:26 +0300

    fix sms comercial loan

--- ls_software/admin/customer_email_message_personal_loan.php
+++ ls_software/admin/customer_email_message_personal_loan.php
@@ -67,38 +67,38 @@ $user_fnd_id = $_GET['user_fnd_id'];
       //CUSTOMER SMS STARTS
       $phone_number = $_GET['mobile_number'];
       $message = "Hello Customer, Please sign the contract here : " . $email_link;
+      send_sms($phone_number,$message);
+      // $curl = curl_init();
 
-      $curl = curl_init();
+      // curl_setopt_array($curl, array(
+      //   CURLOPT_URL => "https://api.sms-magic.com/v1/sms/send",
+      //   CURLOPT_RETURNTRANSFER => true,
+      //   CURLOPT_ENCODING => "",
+      //   CURLOPT_MAXREDIRS => 10,
+      //   CURLOPT_TIMEOUT => 30,
+      //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      //   CURLOPT_HTTPPROXYTUNNEL => TRUE,
+      //   CURLOPT_SSL_VERIFYPEER => FALSE,
+      //   CURLOPT_CUSTOMREQUEST => "POST",
+      //   CURLOPT_POSTFIELDS => "{\r\n\"sms_text\": \"$message\",\r\n\"sender_id\": \"76054\",\r\n\"source\": \"18335210068\",\r\n\"mobile_number\": \"$phone_number\"\r\n\r\n}",
+      //   CURLOPT_HTTPHEADER => array(
+      //     "apikey: 2b731c8cdf8713eae8e30ae382eb43a3",
+      //     "cache-control: no-cache",
+      //     "content-type: application/json",
+      //     "postman-token: 39676d49-3e38-2535-44e6-3a481f4b0808"
+      //   ),
+      // ));
 
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.sms-magic.com/v1/sms/send",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_HTTPPROXYTUNNEL => TRUE,
-        CURLOPT_SSL_VERIFYPEER => FALSE,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => "{\r\n\"sms_text\": \"$message\",\r\n\"sender_id\": \"76054\",\r\n\"source\": \"18335210068\",\r\n\"mobile_number\": \"$phone_number\"\r\n\r\n}",
-        CURLOPT_HTTPHEADER => array(
-          "apikey: 2b731c8cdf8713eae8e30ae382eb43a3",
-          "cache-control: no-cache",
-          "content-type: application/json",
-          "postman-token: 39676d49-3e38-2535-44e6-3a481f4b0808"
-        ),
-      ));
+      // $response = curl_exec($curl);
+      // $err = curl_error($curl);
 
-      $response = curl_exec($curl);
-      $err = curl_error($curl);
+      // curl_close($curl);
 
-      curl_close($curl);
-
-      if ($err) {
-        echo "cURL Error #:" . $err;
-      } else {
-        // echo $response."<hr>";
-      }
+      // if ($err) {
+      //   echo "cURL Error #:" . $err;
+      // } else {
+      //   // echo $response."<hr>";
+      // }
 
       //CUSTOMER SMS ENDS
 
