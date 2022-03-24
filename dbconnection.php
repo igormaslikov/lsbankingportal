d28b4f60d2427fd1932a4da06cca1d02466e226e
Igor <maslikov.igor@gmail.com>
2022-03-25 01:03:55 +0200

    change dbconnection.php to ofsca user

--- dbconnection.php
+++ dbconnection.php
@@ -1,8 +1,14 @@
 <?php 
-	 $db_host = "50.62.151.36";
-	 //$db_host = "localhost";
-	 //$db_host = "161.97.149.121";
-	 $db_user = "dblsuser2021";
-	 $db_pass = "^%D24L*!Ti5%";
-	 $db_name = "dbs57337";
+	//  //$db_host = "50.62.151.36";
+	//  $db_host = "localhost";
+	//  //$db_host = "161.97.149.121";
+	//  $db_user = "dblsuser2021";
+	//  $db_pass = "^%D24L*!Ti5%";
+	//  $db_name = "dbs57337";
+
+	//$db_host = "213.136.93.169";
+	$db_host = "localhost";
+	$db_user = "ki902621_ofsca_portal_user";
+	$db_pass = "(a%Dk?jE0o*e";
+	$db_name = "ki902621_ofsca_portal";
 ?>
