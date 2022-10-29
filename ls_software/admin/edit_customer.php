e1cd427b300bfbed629557ff53b3cdca66e7747e
Igor <maslikov.igor@gmail.com>
2022-10-29 18:05:25 +0300

    add buisness address to edit customer

--- ls_software/admin/edit_customer.php
+++ ls_software/admin/edit_customer.php
@@ -2114,11 +2114,11 @@ has been approved " . $phone_number_update . " and loan term is " . $personal_lo
     };
 
 
-    mysqli_query($con, "UPDATE source_income SET employer_name ='$employer_name_update', work_phone_no='$work_phone_update', net_check_amount='$net_amount_update', direct_deposit='$direct_deposit_update', pay_period='$pay_fre_update', last_pay_date='$last_date_update', next_pay_date='$next_date_update', last_update_by='$u_id',last_update_date='$date',business_type='$business_type_up',business_create='$business_create_up' where user_fnd_id ='$id' ");
+    mysqli_query($con, "UPDATE source_income SET employer_name ='$employer_name_update', work_phone_no='$work_phone_update', net_check_amount='$net_amount_update', direct_deposit='$direct_deposit_update', pay_period='$pay_fre_update', last_pay_date='$last_date_update', next_pay_date='$next_date_update', last_update_by='$u_id',last_update_date='$date',business_type='$business_type_up',business_create='$business_create_up', 'address_b'='$address_b', 'city_b'='$city_b', 'state_b'='$state_b','zip_b'='$zip_b' where user_fnd_id ='$id' ");
     if (if_insert($con)) {
       // *********************************** Employee Info Insertion **********************************************
 
-      $query_emp  = "INSERT INTO source_income (user_fnd_id,employer_name,work_phone_no,net_check_amount,direct_deposit,pay_period,last_pay_date,next_pay_date,creation_date,business_type,business_create)  VALUES ('$id','$employer_name_update','$work_phone_update','$net_amount_update','$direct_deposit_update','$pay_fre_update','$last_date_update','$next_date_update','$date','$business_type_up','$business_create_up')";
+      $query_emp  = "INSERT INTO source_income (user_fnd_id,employer_name,work_phone_no,net_check_amount,direct_deposit,pay_period,last_pay_date,next_pay_date,creation_date,business_type,business_create,address_b,state_b,city_b,zip_b)  VALUES ('$id','$employer_name_update','$work_phone_update','$net_amount_update','$direct_deposit_update','$pay_fre_update','$last_date_update','$next_date_update','$date','$business_type_up','$business_create_up','$address_b','$state_b','$city_b','$zip_b')";
       $result_emp = mysqli_query($con, $query_emp);
       if ($result_emp) {
         //echo "<div class='form'><h3> successfully added in tbl_shipments.</h3><br/></div>";
